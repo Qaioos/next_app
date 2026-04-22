655c03db7e4429c7a09dc33e51f923653e0bd545
Qaioos <legend442005@gmail.com>
2026-04-22 19:24:42 +0300

    feat: add reusable  Logo components with brandiing styyles

---
+++ app/layout/navbar-parts/Logo.tsx
@@ -0,0 +1,13 @@
+import { TfiLayoutGrid3Alt } from "react-icons/tfi"
+
+ 
+const Logo = () => {
+  return (
+            <div className=" flex items-center h-[64px] w-121.92">
+                <TfiLayoutGrid3Alt className="text-blue-600 m-1" />
+                <p className="text-xl font-bold text-blue-600">TeamFlow</p>
+            </div>
+  )
+}
+
+export default Logo
