c0c46b239f7a9d43b98dfe523803a8f457148161
Qaioos <legend442005@gmail.com>
2026-04-23 18:54:07 +0300

    config(next): randomuser.md and daisyui

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images:{
+    remotePatterns:[
+      {
+        protocol:"https",
+        hostname:'img.daisyui.com'
+      },
+      {
+        protocol:"https",
+        hostname:'randomuser.me'
+      }
+    ]
+  }
   /* config options here */
 };
 
