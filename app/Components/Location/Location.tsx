b58709f5135ed8430a0d8d0b45fcd2b596b9095c
Qaioos <legend442005@gmail.com>
2026-04-25 15:56:49 +0300

    Chang The  Show Location

--- app/Components/Location/Location.tsx
+++ app/Components/Location/Location.tsx
@@ -1,23 +1,28 @@
-"use client"
- 
+"use client";
+
 import { Marker } from "react-leaflet";
-import { MapContainer } from "react-leaflet/MapContainer"
+import { MapContainer } from "react-leaflet/MapContainer";
 
- 
-import { TileLayer } from "react-leaflet/TileLayer"
+import { TileLayer } from "react-leaflet/TileLayer";
 
+const Location = ({
+    coordinatesData,
+}: {
+    coordinatesData: { latitude: string; longitude: string } | undefined;
+}) => {
+    const { latitude, longitude } = coordinatesData || {
+        latitude: 51.505,
+        longitude: 41.09,
+    };
+    return (
+        <iframe
+            width="100%"
+            height="200"
+            loading="lazy"
+            className="rounded-lg border"
+            src={`https://maps.google.com/maps?q=${longitude},${latitude}&z=3&output=embed`}
+        ></iframe>
+    );
+};
 
-const Location =  ({ coordinatesData }: { coordinatesData: { latitude: number; longitude: number } | undefined }) => {
-        const {latitude,longitude} = coordinatesData || { latitude: 51.505, longitude: 41.09 };
-  return (
-    <div className="h-40 w-full mt-2 rounded-md overflow-hidden">
-      <MapContainer center={[latitude, longitude]} zoom={3} scrollWheelZoom={false} style={{height: "100%", width: "100%"}}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={[latitude, longitude]} />
-      </MapContainer>
-    </div>
-    
-  )
-}
-
-export default Location
+export default Location;
