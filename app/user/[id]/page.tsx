ff12a075c85f424fc05b3d06e5cb915634cd2fd6
Qaioos <legend442005@gmail.com>
2026-04-25 15:50:53 +0300

    Add the Some Type Safte  , interface User ,  and the card Location

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,37 +1,111 @@
-import { getData } from "@/app/FetchData/DataUsers"
+import { getData } from "@/app/FetchData/DataUsers";
+import Image from "next/image";
 
-interface Props{
-    params :Promise<{ id: string }>
+// React Icons
+import { AiOutlineMail } from "react-icons/ai";
+import { MdOutlinePhone } from "react-icons/md";
+import { TbWorldWww } from "react-icons/tb";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+
+interface Props {
+    params: Promise<{ id: string }>;
 }
 interface User {
     picture: {
-        large: string;
+        large: string |StaticImport |undefined;
     };
     name: {
         first: string;
         last: string;
     };
-    location:{
-        city:string;
-    }
+    location: {
+        city: string;
+        coordinates: {
+            latitude: string;
+            longitude: string;
+        };
+    };
+    email: string;
+    phone: string;
+    login: {
+        username: string;
+    };
 }
 const page = async ({ params }: Props) => {
-    const {id} = await params
-    const  data = await getData() as  { data: User[] | undefined }
-    const  userIndex = Number(id)
+    const { id } = await params;
+
+    const data = (await getData()) as User[] | undefined;
+
+    const userIndex: number = Number(id);
+
     console.log(data);
-     
-  return (
-    <div>
-        <article>
-            <div>{userIndex}</div>
-            <div> name : {data?.[userIndex]?.name?.first} {data?.[userIndex]?.name?.last}</div>
-        </article>
-        <main>
 
-        </main>
-    </div>
-  )
-}
+    return (
+        <div className="p-4 m-2 bg-gray-200">
+            <article className="flex flex-col gap-5">
+                <div>
+                    <div className="card bg-base-100 w-96 shadow-sm border-b-2 ">
+                        <figure>
+                            <Image
+                                src={data?.[userIndex]?.picture.large ?? "/default-image.jpg"}
+                                alt="Shoes"
+                                width={100}
+                                height={100}
+                                className="w-1/3 mt-10"
+                            />
+                        </figure>
+                        <div className="card-body items-center p-2">
+                            <h2 className="card-title ">
+                                {data?.[userIndex]?.name?.first}{" "}
+                                {data?.[userIndex]?.name?.last}
+                            </h2>
+                            <h2>{data?.[userIndex]?.location?.city}</h2>
+                            <hr className="w-full border-gray-300" />
+                            <div className="card-actions p-1 flex-col w-full justify-start">
+                                <p className="flex items-center gap-2">
+                                    <AiOutlineMail className="text-blue-500" />
+                                    {data?.[userIndex]?.email}
+                                </p>
+                                <p className="flex items-center gap-2">
+                                    <MdOutlinePhone className="text-green-500" />
+                                    {data?.[userIndex]?.phone}
+                                </p>
+                                <p className="flex items-center gap-2">
+                                    <TbWorldWww className="text-purple-500" />
+                                    {data?.[userIndex]?.login?.username}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-export default page
+                <div className="location card bg-base-100 w-96 shadow-sm border-b-2">
+                    <div className="card-body items-center p-2">
+                        <h2 className="card-title">Location</h2>
+                        <p>{data?.[userIndex]?.location?.city}</p>
+                        <p>
+                            la:
+                            {data?.[userIndex]?.location.coordinates.latitude}
+                        </p>
+                        <p>
+                            lo:
+                            {data?.[userIndex]?.location.coordinates.longitude}
+                        </p>
+                    </div>
+                    <div className="w-full h-40 overflow-hidden  ">
+                        <iframe
+                            width="100%"
+                            height="200"
+                            loading="lazy"
+                            className="rounded-lg border"
+                            src={`https://maps.google.com/maps?q=${data?.[userIndex]?.location.coordinates.longitude},${data?.[userIndex]?.location.coordinates.latitude}&z=3&output=embed`}
+                        ></iframe>
+                    </div>
+                </div>
+            </article>
+            <main></main>
+        </div>
+    );
+};
+
+export default page;
