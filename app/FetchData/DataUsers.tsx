3aa48f568f9e0885752a5605e27e1de501349bb7
Qaioos <legend442005@gmail.com>
2026-05-03 11:44:48 +0300

    Add the UserArray Names for The Search  , and ching the Api result , don trundom

--- app/FetchData/DataUsers.tsx
+++ app/FetchData/DataUsers.tsx
@@ -1,8 +1,22 @@
+import { User } from "../Types/Types";
+
 export async function getData() {
-    const res = await fetch("https://randomuser.me/api/?results=10",{next :{revalidate:3000}});
+    const res = await fetch("https://randomuser.me/api/?results=10&seed=myProject",{next :{revalidate:3000000} });
     const data = await res.json();
+     getNames(data.results);
     return data.results;
 }
+
+
+
+function getNames(users: User[]) {
+    return users.map((user) => user.name?.first);
+}
+
+export let userArry = getNames(await getData());
+
+
+
 
 export async function getToDos(UserId: number) {
     const res = await fetch(`https://jsonplaceholder.typicode.com/todos?userId=${UserId}`, {next :{revalidate:3000}});
