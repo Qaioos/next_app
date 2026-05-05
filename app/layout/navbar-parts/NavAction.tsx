2cd698a509b9719919861c85d54289ab1fe5ba40
Qaioos <legend442005@gmail.com>
2026-05-05 10:15:35 +0300

    Split nav and add search components

--- app/layout/navbar-parts/NavAction.tsx
+++ app/layout/navbar-parts/NavAction.tsx
@@ -1,22 +1,21 @@
-import SearchBar from '@/app/Components/Shared/PageHeader/SearchOfUsers'
-import React from 'react'
-import { BiBell } from 'react-icons/bi'
-import { IoIosSearch } from 'react-icons/io'
-import { MdOutlineSettings } from 'react-icons/md'
-import { RxAvatar } from 'react-icons/rx'
+import SearchBar from "@/app/Components/Shared/Search/SearchOfUsers";
+import React from "react";
+import { BiBell } from "react-icons/bi";
+import { IoIosSearch } from "react-icons/io";
+import { MdOutlineSettings } from "react-icons/md";
+import { RxAvatar } from "react-icons/rx";
 
 const NavAction = () => {
-  return (
-            <div className="flex items-center space-x-4 flex-wrap cursor-pointer">
-                    <div>
-                      <SearchBar/>
-                    </div>
-                    <BiBell className="h-6 w-6 text-gray-500 pointer-coarse" />
-                    <MdOutlineSettings className="h-6 w-6 text-gray-500" />
-                    <RxAvatar className="h-6 w-6 text-gray-500" />
-
+    return (
+        <div className="flex items-center space-x-4 flex-wrap cursor-pointer">
+            <div>
+                <SearchBar />
             </div>
-  )
-}
+            <BiBell className="h-6 w-6 text-gray-500 pointer-coarse" />
+            <MdOutlineSettings className="h-6 w-6 text-gray-500" />
+            <RxAvatar className="h-6 w-6 text-gray-500" />
+        </div>
+    );
+};
 
-export default NavAction
+export default NavAction;
